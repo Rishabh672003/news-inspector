45700dbdf728a9fa2c0e0d5977539a102e79140f
amit <sutharamit707@gmail.com>
2024-02-16 15:08:38 +0530

    feat: signup-done

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,34 +1,56 @@
 import { useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
 import { app } from "../firebase";
-import OAuth from "./OAuth";
 
 import { GoogleAuthProvider, signInWithPopup, getAuth } from "@firebase/auth";
 import axios from "axios";
 
 const Signup = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      console.log(formData);
+      const res = await axios.post(
+        "http://localhost:3000/api/auth/signup",
+        formData,
+        { withCredentials: true }
+      );
+      setError(false);
+      setLoading(false);
+      if (window && res) {
+        window.location.href = "/home";
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(error.response ? error.response.data.message : error.message);
+    }
+  }
 
   async function handleGoogleClick() {
     try {
       setLoading(true);
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
-
       const result = await signInWithPopup(auth, provider);
-
       const requestBody = {
-        name: result.username,
-        email: result.email,
-        photo: result.photoURL,
+        name: result.user.displayName,
+        email: result.user.email,
+        photo: result.user.photoURL,
       };
-
-      const response = await axios.post("/api/auth/google", requestBody);
+      const response = await axios.post(
+        "http://localhost:3000/api/auth/google",
+        requestBody
+      );
       const data = response.data;
-
       if (window && data) {
         window.location.href = "/home";
       }
@@ -44,37 +66,46 @@ const Signup = () => {
   return (
     <main className="p-2 bg-[#001220] bg-[url('/layered-waves-haikei.svg')] text-gray-50 bg-cover bg-center md:p-0 h-screen flex flex-col md:flex-row md:justify-around justify-center items-center">
       <div className="left drop-shadow-md shadow-gray-300 p-0.5">
-        <h1 className="text-4xl font-semibold">Summareez</h1>
-        <p>Academic insights, summarized!</p>
+        <h1 className="text-4xl font-semibold">News Inspector</h1>
+        <p>tagline...</p>
       </div>
       <div className="form w-full md:w-1/3">
         <form className="flex flex-col gap-2">
           <input
             className="border-b border-gray-400 bg-transparent focus:outline-none p-1"
+            type="text"
+            id="username"
+            onChange={handleChange}
+            required
+            placeholder="username..."
+          />
+          <input
+            className="border-b border-gray-400 bg-transparent focus:outline-none p-1"
             type="email"
-            label="Email address"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            id="email"
+            onChange={handleChange}
             required
             placeholder="email..."
           />
           <input
             className="border-b border-gray-400 bg-transparent focus:outline-none p-1"
             type="password"
-            label="Create password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            id="password"
+            onChange={handleChange}
             required
             placeholder="password..."
           />
           <button
             type="submit"
-            onClick={console.log("hii")}
+            onClick={handleSubmit}
             className="relative rounded-md inline-block py-3 font-medium group">
             <span className="absolute rounded-md inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1.5 translate-y-1.5 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
             <span className="absolute rounded-md inset-0 w-full h-full bg-teal-300 border-2 border-black group-hover:bg-teal-400"></span>
-            <span className="relative rounded-md text-black">Sign In</span>
+            <span className="relative rounded-md text-black">Sign Up</span>
           </button>
+          <p className="text-red-700 mt-5">
+            {error ? error || "Signup failed" : ""}
+          </p>
         </form>
         <hr className="mt-4" />
         <div className="flex justify-around gap-2 mt-2">
