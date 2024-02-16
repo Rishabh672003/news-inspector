a4e3b5fb02f0baf85331cbfa23bafa03f0be9216
amit <sutharamit707@gmail.com>
2024-02-16 14:24:41 +0530

    feat: firebase-api-up

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,12 +1,16 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAIeB7HyKAY4MGNmc6QZHsxsAqPeq1mcYA",
-  authDomain: "astro-fire-69.firebaseapp.com",
-  projectId: "astro-fire-69",
-  storageBucket: "astro-fire-69.appspot.com",
-  messagingSenderId: "503989480152",
-  appId: import.meta.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyC9QSP0l1oSrt5V9LsnTzIqtynEQXwtSuQ",
+  authDomain: "news-inspector-cb555.firebaseapp.com",
+  projectId: "news-inspector-cb555",
+  storageBucket: "news-inspector-cb555.appspot.com",
+  messagingSenderId: "104225491450",
+  appId: "1:104225491450:web:485bf84e50a257eef8bc43",
 };
 
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
