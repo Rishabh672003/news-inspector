321254c8a409d6d281eb52165b4f59e720192f51
amit <sutharamit707@gmail.com>
2024-02-16 16:13:39 +0530

    feat: view-transition

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,11 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: true,
+    }),
+  ],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
 import { app } from "../firebase";
-import { Image } from "astro:assets";
-import google from "../../public/google.svg";
-
-import { GoogleAuthProvider, signInWithPopup, getAuth } from "@firebase/auth";
 import axios from "axios";
+import "/src/base.css";
 
-const Signup = () => {
+const LogIn = () => {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [checked, setChecked] = useState(false);
 
   function handleChange(e) {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -20,21 +18,28 @@ const Signup = () => {
 
     try {
       setLoading(true);
-      console.log(formData);
+
       const res = await axios.post(
-        "http://localhost:3000/api/auth/signup",
-        formData,
+        "http://localhost:3000/api/auth/signin",
+        { ...formData, remember: checked },
         { withCredentials: true }
       );
+      const data = res.data;
+
       setError(false);
       setLoading(false);
-      if (window && res) {
+
+      if (window && data) {
         window.location.href = "/home";
       }
     } catch (error) {
       setLoading(false);
       setError(error.response ? error.response.data.message : error.message);
     }
+  }
+
+  function checkboxToggle() {
+    setChecked(!checked);
   }
 
   async function handleGoogleClick() {
@@ -63,8 +68,6 @@ const Signup = () => {
     }
   }
 
-  // bg-[#2e3440] bg-[#333333] bg-[#001220]
-
   return (
     <main className="p-2 bg-[#001220] bg-[url('/layered-waves-haikei.svg')] text-gray-50 bg-cover bg-center md:p-0 h-screen flex flex-col md:flex-row md:justify-around justify-center items-center">
       <div className="left drop-shadow-md shadow-gray-300 p-0.5">
@@ -73,14 +76,6 @@ const Signup = () => {
       </div>
       <div className="form w-full md:w-1/3">
         <form className="flex flex-col gap-2">
-          <input
-            className="border-b border-gray-400 bg-transparent focus:outline-none p-1"
-            type="text"
-            id="username"
-            onChange={handleChange}
-            required
-            placeholder="username..."
-          />
           <input
             className="border-b border-gray-400 bg-transparent focus:outline-none p-1"
             type="email"
@@ -103,10 +98,14 @@ const Signup = () => {
             className="relative rounded-md inline-block py-3 font-medium group">
             <span className="absolute rounded-md inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1.5 translate-y-1.5 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
             <span className="absolute rounded-md inset-0 w-full h-full bg-teal-300 border-2 border-black group-hover:bg-teal-400"></span>
-            <span className="relative rounded-md text-black">Sign Up</span>
+            <span className="relative rounded-md text-black">Sign In</span>
           </button>
-          <p className="text-red-500 mt-5">
-            {error ? error || "Signup failed" : ""}
+          <div className="flex">
+            <input id="check" type="checkbox" onChange={checkboxToggle} />
+            <p className="text-slate-200 p-2">Remember me</p>
+          </div>
+          <p className="text-red-500">
+            {error ? error || "Something went wrong!" : ""}
           </p>
         </form>
         <hr className="mt-4" />
@@ -125,17 +124,16 @@ const Signup = () => {
                 <span>Loading...</span>
               </div>
             ) : (
-              <div className="flex gap-2 items-center justify-center">
-                <img width={20} src="/google.svg" alt="google" />
+              <div className="flex items-center justify-center">
                 <span>Continue with Google</span>
               </div>
             )}
           </button>
         </div>
         <p className="mt-2 text-sm">
-          Already have an account?{" "}
-          <a className="text-blue-600 underline" href="/sign-in">
-            Sign in
+          New here?{" "}
+          <a className="text-blue-600 underline" href="/">
+            Sign Up
           </a>
         </p>
       </div>
@@ -143,4 +141,4 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+export default LogIn;
