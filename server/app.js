4e0b3f2edf73d388dd15e37b37e504b9c3b76e96
sumitst05 <sumitst05@gmail.com>
2024-02-16 14:23:49 +0530

    udpate: cors

--- server/app.js
+++ server/app.js
@@ -13,22 +13,30 @@ import authRouter from "./routes/auth.js";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:4321",
+      "https://news-inspector-cb555.firebaseapp.com",
+    ],
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 
 app.use("/api", indexRouter);
 app.use("/api/auth", authRouter);
 
 mongoose
-	.connect(process.env.DB_URI)
-	.then(() => {
-		console.log("Connected to database");
-	})
-	.catch((error) => {
-		console.error(error);
-	})
-	.finally(() => {
-		app.listen(port, () => {
-			console.log(`Server listening on port ${port}...`);
-		});
-	});
+  .connect(process.env.DB_URI)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => {
+    console.error(error);
+  })
+  .finally(() => {
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}...`);
+    });
+  });
