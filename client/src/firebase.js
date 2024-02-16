7a9e66cc497997200be7c318b0eca2fd4a58d5ad
amit <sutharamit707@gmail.com>
2024-02-16 17:21:17 +0530

    feat: env

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,15 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC9QSP0l1oSrt5V9LsnTzIqtynEQXwtSuQ",
+  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
   authDomain: "news-inspector-cb555.firebaseapp.com",
   projectId: "news-inspector-cb555",
   storageBucket: "news-inspector-cb555.appspot.com",
   messagingSenderId: "104225491450",
-  appId: "1:104225491450:web:485bf84e50a257eef8bc43",
+  appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
